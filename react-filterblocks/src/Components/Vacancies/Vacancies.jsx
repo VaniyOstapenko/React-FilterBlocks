5007b44c027c3789ea70dc75d21560bb5e08f6c1
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-03-01 23:05:40 +0300

    React-FilterBlocks

--- react-filterblocks/src/Components/Vacancies/Vacancies.jsx
+++ react-filterblocks/src/Components/Vacancies/Vacancies.jsx
@@ -3,12 +3,21 @@ import { Pagination } from "@mantine/core";
 import { IconMapPin } from "@tabler/icons-react";
 import array from "../../Storage/Storage.json";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function Vacancies() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [foundVacancyPage] = useState(3);
+
+  const lastVacansy = currentPage * foundVacancyPage;
+  const firstVacancy = lastVacansy - foundVacancyPage;
+  const currentVacancy = array.slice(firstVacancy, lastVacansy);
+
   return (
     <>
       <div className={style.vacancies}>
         <div className={style.list}>
-          {array.map((el) => (
+          {currentVacancy.map((el) => (
             <Link to={`/${el.id}`}>
               <div key={el.id} className={style.content}>
                 <h1>{el.title}</h1>
@@ -25,7 +34,11 @@ function Vacancies() {
             </Link>
           ))}
         </div>
-        <Pagination total={10} />
+        <Pagination
+          total={Math.ceil(array.length / currentPage)}
+          onChange={setCurrentPage}
+          mt="sm"
+        />
       </div>
     </>
   );
